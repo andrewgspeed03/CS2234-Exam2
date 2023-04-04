6d7434d8239c5d0d240d02178e86b05423691cbc
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-04-04 15:46:00 -0500

    Add files via upload
    Completed Sorted Age Queue

--- src/AgeQueue.java
+++ src/AgeQueue.java
@@ -4,17 +4,36 @@ import java.util.*;
 public class AgeQueue {
 
 	//fill
+	private Queue<Person> people = new LinkedList<>();
 
 	public void addPerson(Person person) {
-
 		//fill
+		if(!isEmpty()) {
+			int compare = person.compareTo(people.peek());
+			if(compare > 0) {
+				Person temp = removePerson();
+				people.offer(person);
+				people.offer(temp);
+			}
+			else {
+				people.offer(person);
+			}
+		}
+		else {
+			people.offer(person);
+		}
 	}
 
 	public Person removePerson() {
 		//fill
+		if(!isEmpty())
+			return people.remove();
+		else
+			return null;
 	}
 
 	public boolean isEmpty() {
 		//fill
+		return people.isEmpty(); 
 	}
 }
